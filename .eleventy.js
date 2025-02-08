15a9557fb1aa74dacbb570ffbde466968e51107c
jessiebeaudoin <123115086+jessiebeaudoin@users.noreply.github.com>
2025-02-08 18:52:35 +0000

    changes to passthrough file copies

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,11 @@
 module.exports = function(eleventyConfig) {
+  // Pass through static assets to the output folder
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
 
   // Create a collection for planets
-  eleventyConfig.addCollection("planets", function(collection) {
-      return collection.getFilteredByGlob("planets/*.md");
+  eleventyConfig.addCollection("planets", function(collectionApi) {
+      return collectionApi.getFilteredByGlob("planets/*.md");
   });
 
   return {
