456b0cdc903abf3f5f1152b97a8474a40164d060
jessiebeaudoin <123115086+jessiebeaudoin@users.noreply.github.com>
2025-02-08 18:46:40 +0000

    modifying 11ty to recognize planet files

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,12 @@
-
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
-  
+
+  // Create a collection for planets
+  eleventyConfig.addCollection("planets", function(collection) {
+      return collection.getFilteredByGlob("planets/*.md");
+  });
+
   return {
       dir: {
           input: ".",
