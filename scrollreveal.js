dee58a935fd18068b608b3d682b565995a73e5b1
jessiebeaudoin <123115086+jessiebeaudoin@users.noreply.github.com>
2025-02-08 18:30:22 +0000

    adding animation on cards

---
+++ scrollreveal.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    ScrollReveal().reveal('.planet-card', { 
+        duration: 1000,
+        origin: 'bottom',
+        distance: '50px'
+    });
+});
